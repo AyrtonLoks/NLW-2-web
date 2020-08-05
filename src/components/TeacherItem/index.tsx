859f9a56fb73c4f2d5b3cd6c75a5304796804fb9
Ayrton Neves <ayrton.leandro@gmail.com>
2020-08-05 20:59:02 -0300

    Dia 3 ok

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,35 +1,57 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/52423106?s=460&u=b7be48e5ea2534cc86cde0219e07d7dd14ab39a0&v=4" alt=""/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Zé Pilintra</strong>
-                    <span>Putaria desenfreada</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Quando criança sempre pensei em comer todo mundo que aparecesse na minha frente, pois bem, hoje faço isso.
-                <br/><br/>
-                Agora, como a minha conta bancária está repleta de dinheiro, e, eu de doenças sexualmente transmissíveis, estou ensinando outras pessoas a terem o que desejam.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 100,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
